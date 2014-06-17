666fb324aa9f0939102e21d5d18010b9e11e7c3b
Adam D <kirbylover4000@gmail.com>
2014-06-17 21:14:44 +0930

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# WallpaperHaxMinus
+
+**The stupid gets stupider.**
+
+Just loads the [WallpaperHaxPlus](https://github.com/kirb/WallpaperHaxPlus) DLL and ensures it's set to run at logon. ClickOnce is pretty cool.
+
+[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0.html)
---
+++ WallpaperHaxMinus/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WallpaperHaxPlus;
+
+namespace WallpaperHaxMinus
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\PCHax Technology\\WallpaperHaxMinus", "LastVersion", 1, RegistryValueKind.DWord);
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "WallpaperHaxMinus", Process.GetCurrentProcess().MainModule.FileName);
+
+			Application.Run(new WallpaperHaxPlus.WallpaperForm());
+		}
+	}
+}
